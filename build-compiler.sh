8d7c294c306e0945c42968de8eaa5dc38ff7faad
Patrick Ferris <patrickferris17@gmail.com>
2020-01-25 19:55:11 +0000

    Create compiler builder script

---
+++ build-compiler.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo "Building compiler for host (x86...) with no optimisations"
+git checkout 88b3b40c87f7d2c239620a04bdf715ab3abdf7c3  
+./configure -no-ocamldoc -no-debugger -prefix /riscv-ocaml && make -j8 world.opt && make install 
+export PATH="/riscv-ocaml/bin:${PATH}"
+# Checkout the latest code with the inline assembly 
+echo "Building compiler for target (riscv64-unknown-linux-gnu) with optimisations - using commit hash supplied"
+git checkout $0 && git stash apply 
+make clean && ./configure --target riscv64-unknown-linux-gnu -prefix /riscv-ocaml -no-ocamldoc -no-debugger -target-bindir /riscv-ocaml/bin && make -j8 world || /bin/true 
+make -j8 opt
+cp /riscv-ocaml/bin/ocamlrun byterun
+make install
