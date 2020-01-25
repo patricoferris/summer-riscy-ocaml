7457f0ceefb6fc864c912e78d9adf2beac8f44ef
Patrick Ferris <patrickferris17@gmail.com>
2020-01-25 20:16:02 +0000

    Fix argument param

--- build-compiler.sh
+++ build-compiler.sh
@@ -5,7 +5,7 @@ git checkout 88b3b40c87f7d2c239620a04bdf715ab3abdf7c3
 export PATH="/riscv-ocaml/bin:${PATH}"
 # Checkout the latest code with the inline assembly 
 echo "Building compiler for target (riscv64-unknown-linux-gnu) with optimisations - using commit hash supplied"
-git checkout $0 && git stash apply 
+git checkout $1 && git stash apply 
 make clean && ./configure --target riscv64-unknown-linux-gnu -prefix /riscv-ocaml -no-ocamldoc -no-debugger -target-bindir /riscv-ocaml/bin && make -j8 world || /bin/true 
 make -j8 opt
 cp /riscv-ocaml/bin/ocamlrun byterun
